6b3704f95b2d89b09d2b5748d0dda33a4b546b9f
Raihan Sarkar <bdyoungdeveloper@gmail.com>
2022-06-09 00:42:21 +0600

    home page banner first container finished

--- src/components/Home/HomeBanner.jsx
+++ src/components/Home/HomeBanner.jsx
@@ -3,8 +3,30 @@ import React from "react";
 const Banner = () => {
     return (
         <>
-            <div className="px-[10%] grid grid-cols-3 gap-5">
+            <div className="px-[80px] flex h-[564px] justify-between gap-10">
                 {/* banner left 1st conatiner  */}
+                <div className="bg-[#F5F5F5] rounded-lg relative w-[270px] h-full text-center py-[48px]">
+                    <div className="mb-auto">
+                        <h1 className="text-sm font-semibold">Asus</h1>
+                        <h1 className="text-xl my-[8px] font-semibold">
+                            Supper Sale
+                        </h1>
+                        <h1 className="text-xl font-semibold">Laptop Gaming</h1>
+                    </div>
+                    <div className="absolute left-0 right-0  bottom-[48px]">
+                        <span class="btn text-center  rounded-lg btn-primary text-white text-[14px] px-[16px] py-[8px]">
+                            Order Now
+                        </span>
+                    </div>
+                </div>
+                {/* banner left 2nd conatiner  */}
+                <div className=" bg-red-500   w-full h-[100%]">
+                    <h1>Yay I am secaond</h1>
+                </div>
+                {/* banner left 3rd conatiner  */}
+                <div className="bg-pink-600 w-[368px] h-[100%]">
+                    <h1>Yay I am third</h1>
+                </div>
             </div>
         </>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+                    primary: "#68D237",
+                    secondary: "#f6d860",
+                    accent: "#37cdbe",
+                    neutral: "#3d4451",
+                    "base-100": "#ffffff",
+                },
+            },
+            "dark",
+            "cupcake",
+        ],
+    },
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [require("daisyui")],
 };
