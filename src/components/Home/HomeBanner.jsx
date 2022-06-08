075534388a20111b43dd198742cf5c789bd4912e
Raihan Sarkar <bdyoungdeveloper@gmail.com>
2022-06-08 23:31:34 +0600

    basics setup completed and home page banner section started

---
+++ src/components/Home/HomeBanner.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Banner = () => {
+    return (
+        <>
+            <div className="px-[10%] grid grid-cols-3 gap-5">
+                {/* banner left 1st conatiner  */}
+            </div>
+        </>
+    );
+};
+
+export default Banner;
