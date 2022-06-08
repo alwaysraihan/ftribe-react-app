3aeabdda42d8a7533d4addaf042dfd38ef0a9a7b
Raihan Sarkar <bdyoungdeveloper@gmail.com>
2022-06-09 01:04:21 +0600

    Navbar basics ui added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Home from "./pages/Home";
 function App() {
     return (
         <>
-            <h1>Hello</h1>
             <Home />
         </>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Banner from "../components/Home/HomeBanner";
+import Navbar from "../components/Shared/Navbar";
 
 const Home = () => {
     return (
         <>
+            <Navbar />
             <Banner />
         </>
     );
